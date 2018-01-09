ca5308cfb4bd2664b5a1b94a21fbf80881e83e8a
Robert Axelsen <rob@rob.ee>
2018-01-09 22:03:36 +0100

    Start using gorilla/mux and encoding/json

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
+var commands = []Command{Command{"tar", "an archiving utility"}}
+
+type Command struct {
+	Title       string
+	Description string
+}
+
 func main() {
-	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, API!")
-	})
+	r := mux.NewRouter()
+	r.HandleFunc("/commands", getCommandsHandler).Methods("GET")
+	http.Handle("/", r)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func getCommandsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	j, _ := json.Marshal(commands)
+	w.Write(j)
 }
